bf0eb2747ec545e60fa6f857407446f6bce614a6
ivafka <ivanbarhatov480@gmail.com>
2026-04-17 14:51:53 +0700

    chore: setup solution structure with 5 projects and 3 test projects

---
+++ src/StockTracker.Worker/Program.cs
@@ -0,0 +1,7 @@
+using StockTracker.Worker;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
---
+++ tests/StockTracker.ArchTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace StockTracker.ArchTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
