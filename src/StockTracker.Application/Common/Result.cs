ce3c0168b81bbc49faf920244f25a83c8f847415
ivafka <ivanbarhatov480@gmail.com>
2026-04-24 00:45:33 +0700

    feat: add Application layer base (Result, interfaces)

---
+++ src/StockTracker.Application/Common/Result.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StockTracker.Application.Common
+{
+    internal class Result
+    {
+    }
+}
---
+++ src/StockTracker.Application/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StockTracker.Application.Interfaces
+{
+    internal interface IUnitOfWork
+    {
+    }
+}
