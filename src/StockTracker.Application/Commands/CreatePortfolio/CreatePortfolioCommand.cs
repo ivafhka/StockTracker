fcbff167a12c70b9d8458cb31d94e316f47ce4dc
ivafka <ivanbarhatov480@gmail.com>
2026-04-25 15:12:29 +0700

    feat: add CreatePortfolio command

---
+++ src/StockTracker.Application/Commands/CreatePortfolio/CreatePortfolioCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using StockTracker.Application.Common;
+
+namespace StockTracker.Application.Commands.CreatePortfolio
+{
+    public record CreatePortfolioCommand(
+        Guid UserId,
+        string Name, 
+        string? Description) : IRequest<Result<Guid>>;
+}
